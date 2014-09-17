dbca77a95ca04ceae85e0f1238ed41bd01a50f25
Christopher Murphy <christmurphy@gmail.com>
2014-09-17 04:53:56 -0600

    this is a second commit since I have gotten a bit of grasp on gitHub. other than that there are two classes that don't really do anything but print a few lines of text.

---
+++ src/Ingredient.java
@@ -0,0 +1,14 @@
+/**
+ * Created by newScanTron on 9/17/2014.
+ */
+public class Ingredient {
+    public void Ingreient(String name){
+        System.out.println("this is your name");
+
+    }
+    public String printIt()
+    {
+          return "returned";
+
+    }
+}
---
+++ src/recipeList.java
@@ -0,0 +1,14 @@
+
+/**
+ * Created by DaTaMoNsTeReR on 9/9/2014 at 21:07.
+ */
+public class recipeList {
+    public static void main(String[] args) {
+        System.out.println("Recipe App");
+        Ingredient ingredient = new Ingredient();
+        System.out.println(ingredient.printIt());
+
+    }
+
+}
+
