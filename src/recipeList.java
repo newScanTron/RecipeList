51f003c1631004dcdb1461c8640547a71fdd8510
aaroncameron21 <aaroncameron21@gmail.com>
2014-09-26 12:57:26 -0600

    Added framework in Recipe and Ingredient classes
    Built a simple Recipe and Ingredient class to get them started. Still
    not sure how the SQL is going to be incorporated into this so some of
    this may eventually need to be changed or completely removed. Still
    getting used to github, so I’m sorry if I messed anything up. Let me
    know if I did so that way I can avoid it next time.

--- src/recipeList.java
+++ src/recipeList.java
@@ -1,15 +1,37 @@
-
+import java.util.ArrayList;
 /**
  * Created by DaTaMoNsTeReR on 9/9/2014 at 21:07.
  */
 public class recipeList {
     public static void main(String[] args) {
         System.out.println("Recipe App");
-        Ingredient ingredient = new Ingredient();
-        System.out.println(ingredient.printIt());
+        testRecipeClass();
+
+
+
+
+
         
 
     }
 
+    public static void testRecipeClass() {
+        Ingredient testIng = new Ingredient(10,"stuff","2","cups");
+        Ingredient[] testIngList = new Ingredient[] {testIng};
+
+        ArrayList<String> testTags = new ArrayList<String>();
+        testTags.add("Unreal");
+        testTags.add("Steazy");
+
+        Recipe testRec = new Recipe(20,"Thing",new Ingredient[] {testIng}, new String[] {"Prepare stuff","Cook stuff","Serve stuff"}, testTags);
+        testRec.addTag("Super");
+        testRec.addTag("Unreal");
+        System.out.println(testRec);
+    }
+
+
+
 }
+
+
 
